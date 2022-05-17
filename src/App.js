4cf8650819f4f5889f041d09f875d5861e3a6ea5
Jason Zhao <jasonnope123@gmail.com>
2022-05-16 18:03:32 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <p1 class = "corner">Work</p1>
+        <p1 class = "corner">About</p1>
+      </div>
+      <div class="content">
+        <h1 class ="contentText">Hello, I'm Jason.
+        <h1 class="contentText">I am a student.</h1>
+        </h1>
+        <p1 class="contentPara">I am a student at Point Grey Secondary school</p1>
+      </div>
+      <img class="image" src='https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_760x427.v1605559903.png' alt="" height="100px"></img>
     </div>
-  );
-}
 
+  );
+  
+}
 export default App;
