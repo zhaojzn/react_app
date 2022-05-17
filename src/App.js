965b81d6633aee0ca26ca1bb9a9dfc22f78ce976
Jason Zhao <jasonnope123@gmail.com>
2022-05-16 18:03:39 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const App = () => {
         </h1>
         <p1 class="contentPara">I am a student at Point Grey Secondary school</p1>
       </div>
-      <img class="image" src='https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_760x427.v1605559903.png' alt="" height="100px"></img>
     </div>
 
   );
