ff803ec9407610bfe6ea5724211c79c3c5aa96b7
Jason Zhao <jasonnope123@gmail.com>
2022-05-23 19:20:29 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,19 +1,29 @@
 import React from "react";
+import axios from "axios";
+
+const fetchList = async () =>{
+  const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  return res;
+}
 
 class Test extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { seconds: 0 };
+      this.state = { seconds: 0, test: "Test", json: ""};
+      
     }
-  
+    
     tick() {
       this.setState(state => ({
         seconds: state.seconds + 1
+        
       }));
     }
   
+    
     componentDidMount() {
       this.interval = setInterval(() => this.tick(), 1000);
+      this.json = fetchList();
     }
   
     componentWillUnmount() {
@@ -24,6 +34,9 @@ class Test extends React.Component {
       return (
         <div>
           Seconds: {this.state.seconds}
+          <br></br>
+          Variable: {this.state.test}
+          <br></br>
         </div>
       );
     }
